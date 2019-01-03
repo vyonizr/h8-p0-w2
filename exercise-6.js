3ae383812c00ffdbe14429929e5341855aaad29a
vyonizr <fitrahtur@gmail.com>
2019-01-03 09:40:48 +0700

    Added "else" on exercise-6.js

--- exercise-6.js
+++ exercise-6.js
@@ -49,6 +49,9 @@ for(i=1; i <= 100; i += 2) {
   if (i % 3 === 0) {
     console.log(i + " KELIPATAN 3");
   }
+  else {
+    console.log(i);
+  }
 }
 
 console.log("------------------");
@@ -58,6 +61,9 @@ for(i=1; i <= 100; i += 5) {
   if (i % 6 === 0) {
     console.log(i + " KELIPATAN 6");
   }
+  else {
+    console.log(i);
+  }
 }
 
 console.log("------------------");
@@ -66,5 +72,8 @@ console.log("PERULANGAN COUNTER 9");
 for(i=1; i <= 100; i += 9) {
   if (i % 10 === 0) {
     console.log(i + " KELIPATAN 10");
+  }
+  else {
+    console.log(i);
   }
 }
